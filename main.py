85f8415e072ec461ec3596b11871e1053d2cd628
Drochec <hdspawn104@gmail.com>
2022-12-07 19:50:49 +0100

    Brzdeni pred prekazkou
    +Ultrasonic
    +Fce kontrola

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ drive = Motor(Port.C)
 steering = Motor(Port.B)
 infra = InfraredSensor(Port.S1)
 compass = Ev3devSensor(Port.S2)
+ultra = UltrasonicSensor(Port.S3)
 
 #Vars
 running = False
 canSteerL = True
 canSteerR = True
+canForward = True
 maxAngle = 30
 minAngel = -30
 
@@ -26,12 +28,12 @@ while True:
     buttons = infra.keyboard()
     if len(buttons)>1:
         if isinstance(buttons[0], Button.Left_UP) and isinstance(buttons[1], Button.Right_UP):
-            if running:
-                drive.stop()
-                running = False
-            else:
+            if (not running) and canForward:
                 drive.run(50)
                 running = True
+            else:
+                drive.stop()
+                running = False
         if isinstance(buttons[0], Button.Left_DOWN) and isinstance(buttons[1], Button.Right_DOWN):
             if running:
                 drive.stop()
@@ -62,3 +64,19 @@ def steerCheck():
         steering.brake()
     else:
         canSteerR = True
+
+def EStop():
+    distance = ultra.distance()
+    if distance < 30:
+        ev3.speaker.beep()
+        ev3.light.on(Color.GREEN)
+        canDrive = True
+    elif distance < 20:
+        ev3.speaker.beep(600)
+        ev3.light.on(Color.ORANGE)
+        canDrive = True
+    elif distance < 10:
+        ev3.speaker.beep(750)
+        ev3.light.on(Color.RED)
+        canDrive = False
+        drive.brake()
